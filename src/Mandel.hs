07e468859502cd0b740f0ab3c66aecc30079fa98
cesipy <cedric.sillaber@gmail.com>
2023-10-08 15:30:29 +0200

    generate complex plane

--- src/Mandel.hs
+++ src/Mandel.hs
@@ -5,6 +5,7 @@ import Data.Complex
 mandelbrotFunction:: Complex Double -> Complex Double -> Complex Double
 mandelbrotFunction z_n c = z_n * z_n + c
 
+
 -- calculate the amount of complex number z
 magnitudeComplexNumber :: Complex Double -> Double
 magnitudeComplexNumber z = sqrt (a * a + b * b)
@@ -13,7 +14,16 @@ magnitudeComplexNumber z = sqrt (a * a + b * b)
         b :: Double = imagPart z
 
 
+mandelbrotSetMembershipCheck :: Int -> Complex Double -> Complex Double -> Maybe(Complex Double)
+mandelbrotSetMembershipCheck 0 z c = Just z
+mandelbrotSetMembershipCheck n z c 
+    | magnitudeComplexNumber z >= 2 = Nothing
+    | otherwise = mandelbrotSetMembershipCheck (n-1) (mandelbrotFunction z c) c
 
 
-iterateNTimes 0 z c = z
-iterateNTimes n z c = iterateNTimes (n-1) (mandelbrotFunction z c) c
+generateComplexPlaneGrid ::Double -> Int -> [Complex Double]
+--generateList n step = [0, step..n]
+generateComplexPlaneGrid step n = 
+    [x :+ y | x <- [0, step..(fromIntegral n)], y <- [0, step..(fromIntegral n)]]
+
+
--- src/test.hs
+++ src/test.hs
@@ -1,8 +1,11 @@
 import Mandel
 import Data.Complex
+--import Graphics
 
 
 mandelTest = mandelbrotFunction z_0 c
     where
         z_0:: Complex Double = 0.0 :+ 0
         c  :: Complex Double = 1.0 :+ 1.2
+
+
