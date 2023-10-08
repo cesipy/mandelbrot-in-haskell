0a8891a85931135c49312f4b322892df2780bb9e
cesipy <cedric.sillaber@gmail.com>
2023-10-08 15:58:33 +0200

    iterate over the grid of number

--- src/Mandel.hs
+++ src/Mandel.hs
@@ -2,6 +2,7 @@ module Mandel where
 
 import Data.Complex
 
+
 mandelbrotFunction:: Complex Double -> Complex Double -> Complex Double
 mandelbrotFunction z_n c = z_n * z_n + c
 
@@ -14,11 +15,11 @@ magnitudeComplexNumber z = sqrt (a * a + b * b)
         b :: Double = imagPart z
 
 
-mandelbrotSetMembershipCheck :: Int -> Complex Double -> Complex Double -> Maybe(Complex Double)
-mandelbrotSetMembershipCheck 0 z c = Just z
+mandelbrotSetMembershipCheck :: Int -> Complex Double -> Complex Double -> Bool
+mandelbrotSetMembershipCheck 0 z c  = True
 mandelbrotSetMembershipCheck n z c 
-    | magnitudeComplexNumber z >= 2 = Nothing
-    | otherwise = mandelbrotSetMembershipCheck (n-1) (mandelbrotFunction z c) c
+    | magnitudeComplexNumber z >= 2 = False
+    | otherwise                     = mandelbrotSetMembershipCheck (n-1) (mandelbrotFunction z c) c
 
 
 generateComplexPlaneGrid ::Double -> Int -> [Complex Double]
@@ -27,3 +28,9 @@ generateComplexPlaneGrid step n =
     [x :+ y | x <- [0, step..(fromIntegral n)], y <- [0, step..(fromIntegral n)]]
 
 
+iterateOverComplexGrid :: [Complex Double] -> [Bool]
+iterateOverComplexGrid []     = []
+iterateOverComplexGrid (x:xs) = mandelbrotSetMembershipCheck iterations z_0 x : iterateOverComplexGrid xs
+    where
+        iterations            = 10
+        z_0:: Complex Double  = 0.0 :+ 0.0
