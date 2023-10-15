262e464dc9ab6f9d34498d3f651e6391c07bcf9e
Cedric Sillaber <cedric.sillaber@gmail.com>
2023-10-15 19:30:22 +0200

    add documentation

--- src/Mandel.hs
+++ src/Mandel.hs
@@ -16,53 +16,6 @@ magnitudeComplexNumber z = sqrt (a * a + b * b)
         b :: Double = imagPart z
 
 
-
-
-mandelbrotSetMembershipCheck :: Int -> Complex Double -> Complex Double -> Bool
-mandelbrotSetMembershipCheck 0 z c  = True
-mandelbrotSetMembershipCheck n z c
-    | magnitudeComplexNumber z >= 2 = False
-    | otherwise                     = mandelbrotSetMembershipCheck (n-1) (mandelbrotFunction z c) c
-
-
-
-generatePixel :: Double -> Double -> PixelRGB8
-generatePixel x y = generateRGB ( calculateColor maxIterations z_0 ( createComplexNumber x y) 0)
-    where
-        maxIterations = 110
-        z_0           = createComplexNumber 0 0
-
-
-createComplexNumber :: Double ->  Double -> Complex Double
-createComplexNumber x y = x :+ y
-
-
-calculateColor :: Int ->  Complex Double ->Complex Double -> Int -> Int
-calculateColor iterations z_n c i
-    | i > iterations                 = 0
-    | magnitudeComplexNumber z_n >= 2.0 = 255 -  adjustSpan i iterations
-    | otherwise                      = calculateColor iterations (mandelbrotFunction z_n c) c (i+1)
-
-
-adjustSpan :: Int -> Int -> Int
-adjustSpan i maxIterations = i *  255 `div` maxIterations 
-
-
-generateRGB :: Int -> PixelRGB8
-generateRGB x = PixelRGB8 (fromIntegral x) (fromIntegral x) (fromIntegral x)
-
-
-x_min = -0.31
-x_max = -0.35
-y_min = -0.60
-y_max = -0.64
-
-width  :: Int
-width  = 3000
-
-height :: Int
-height = 3000
-
 mapToBoundries:: Int -> Int -> (Double, Double)
 mapToBoundries x y  =
             let newX  = x_min + fromIntegral x * ((x_max - x_min) / fromIntegral width)
@@ -75,5 +28,63 @@ renderFunction x y =  generatePixel (fst mappedPosition) (snd mappedPosition)
     where
         mappedPosition = mapToBoundries x y
 
+
+
+
+-- | Checks whether a complex number is a member of the Mandelbrot Set.
+--
+-- Given a starting point, a complex number, and an iteration counter this function
+-- checks if the complex number remains within the Mandelbrot Set after a series
+-- of iterations. 
+mandelbrotSetMembershipCheck :: Int           -- ^ Number of iterations
+                            -> Complex Double -- ^ Current complex number
+                            -> Complex Double -- ^ Initial complex number
+                            -> Bool
+mandelbrotSetMembershipCheck 0 _ _  = True
+mandelbrotSetMembershipCheck n z c
+    | magnitudeComplexNumber z >= 2 = False
+    | otherwise                     = mandelbrotSetMembershipCheck (n-1) (mandelbrotFunction z c) c
+
+
+-- | Generates pixel for spedific x and y coordinates
+generatePixel :: Double -> Double -> PixelRGB8
+generatePixel x y = generateRGB ( calculateColor maxIterations z_0 ( createComplexNumber x y) 0)
+    where
+        maxIterations = 170
+        z_0           = createComplexNumber 0 0
+
+
+-- | Generates a complex number.
+createComplexNumber :: Double ->  Double -> Complex Double
+createComplexNumber x y = x :+ y
+
+
+-- | Calculates the color for a complex number based on its position in the mandelbrot set.
+calculateColor :: Int ->  Complex Double ->Complex Double -> Int -> Int
+calculateColor iterations z_n c i
+    | i > iterations                 = 0
+    | magnitudeComplexNumber z_n >= 2.0 = 255 -  adjustSpan i iterations
+    | otherwise                      = calculateColor iterations (mandelbrotFunction z_n c) c (i+1)
+
+
+-- | Adjusts a number `i` to a specified range. (`maxBoundries`)
+adjustSpan :: Int -> Int -> Int
+adjustSpan i maxBoundries = i *  255 `div` maxBoundries
+
+-- | Generates a Pixel with type `PixelRGB8` with RGB value `(x, x, x)`
+generateRGB :: Int -> PixelRGB8
+generateRGB x = PixelRGB8 (fromIntegral x) (fromIntegral x) (fromIntegral x)
+
+
+x_min =  -0.75
+x_max = -0.745
+y_min = 0.1
+y_max = 0.105
+
+width  :: Int
+width  = 4000
+
+height :: Int
+height = 4000
 
 
