9ceea5fd1ea223eda77b32aad72b8d020eab29b9
Cedric Sillaber <cedric.sillaber@gmail.com>
2023-10-18 20:33:50 +0200

    refactoring

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,11 @@
 module Main where
 
 import Mandel
-import qualified Data.ByteString.Lazy as BL
 import Codec.Picture
 
 
 
 main :: IO ()
 main = do
-   -- BL.writeFile "results.csv" csvData 
+   let useColors::Bool = True
    writePng "result.png" $ generateImage renderFunction width height
--- src/Mandel.hs
+++ src/Mandel.hs
@@ -3,6 +3,22 @@ module Mandel where
 import Data.Complex
 import Codec.Picture ( PixelRGB8(..) )
 
+{------------------------------------------------}
+
+x_min =  -0.75
+x_max = -0.745
+y_min = 0.1
+y_max = 0.105
+
+width  :: Int
+width  = 2000
+
+height :: Int
+height = 2000
+
+{------------------------------------------------}
+
+
 -- | Calculates the next iteration of `z_n+1` with a given `z_n` and  `c`
 mandelbrotFunction:: Complex Double -> Complex Double -> Complex Double
 mandelbrotFunction z_n c = z_n * z_n + c
@@ -14,22 +30,6 @@ magnitudeComplexNumber z = sqrt (a * a + b * b)
     where
         a :: Double = realPart z
         b :: Double = imagPart z
-
-
--- | Maps `x`and `y` to a given `width` and `height`
-mapToBoundries:: Int -> Int -> Int -> Int -> (Double, Double)
-mapToBoundries x y width height  =
-            let newX  = x_min + fromIntegral x * ((x_max - x_min) / fromIntegral width)
-                newY  = y_max - fromIntegral y * ((y_max - y_min) / fromIntegral height)
-            in (newX, newY)
-
-
-renderFunction :: Int -> Int -> PixelRGB8
-renderFunction x y =  generatePixel (fst mappedPosition) (snd mappedPosition)
-    where
-        mappedPosition = mapToBoundries x y width height
-
-
 
 
 -- | Checks whether a complex number is a member of the Mandelbrot Set.
@@ -47,17 +47,14 @@ mandelbrotSetMembershipCheck n z c
     | otherwise                     = mandelbrotSetMembershipCheck (n-1) (mandelbrotFunction z c) c
 
 
--- | Generates pixel for spedific x and y coordinates
-generatePixel :: Double -> Double -> PixelRGB8
-generatePixel x y = generateRGB ( calculateColor maxIterations z_0 ( createComplexNumber x y) 0)
-    where
-        maxIterations = 170
-        z_0           = createComplexNumber 0 0
-
-
 -- | Generates a complex number.
 createComplexNumber :: Double ->  Double -> Complex Double
 createComplexNumber x y = x :+ y
+
+
+-- | Adjusts a number `i` to a specified range. (`maxBoundries`)
+adjustSpan :: Int -> Int -> Int
+adjustSpan i maxBoundries = i *  255 `div` maxBoundries
 
 
 -- | Calculates the color for a complex number based on its position in the mandelbrot set.
@@ -68,24 +65,33 @@ calculateColor iterations z_n c i
     | otherwise                      = calculateColor iterations (mandelbrotFunction z_n c) c (i+1)
 
 
--- | Adjusts a number `i` to a specified range. (`maxBoundries`)
-adjustSpan :: Int -> Int -> Int
-adjustSpan i maxBoundries = i *  255 `div` maxBoundries
-
 -- | Generates a Pixel with type `PixelRGB8` with RGB value `(x, x, x)`
 generateRGB :: Int -> PixelRGB8
 generateRGB x = PixelRGB8 (fromIntegral x) (fromIntegral x) (fromIntegral x)
 
 
-x_min =  -0.75
-x_max = -0.745
-y_min = 0.1
-y_max = 0.105
-
-width  :: Int
-width  = 4000
-
-height :: Int
-height = 4000
+-- | Generates a Pixel with type `PixelRGB8` with RGB value `(x, y, z)`
+generateRGBdifferentColors :: Int -> PixelRGB8
+generateRGBdifferentColors x = PixelRGB8 (fromIntegral x) (fromIntegral x `div` 3) (fromIntegral x `div` 2)
 
 
+-- | Generates pixel for spedific x and y coordinates
+generatePixel :: Double -> Double -> PixelRGB8
+generatePixel x y = generateRGBdifferentColors ( calculateColor maxIterations z_0 ( createComplexNumber x y) 0)
+    where
+        maxIterations = 170
+        z_0           = createComplexNumber 0 0
+
+
+-- | Maps `x`and `y` to a given `width` and `height`
+mapToBoundries:: Int -> Int -> Int -> Int -> (Double, Double)
+mapToBoundries x y width height  =
+            let newX  = x_min + fromIntegral x * ((x_max - x_min) / fromIntegral width)
+                newY  = y_max - fromIntegral y * ((y_max - y_min) / fromIntegral height)
+            in (newX, newY)
+
+
+renderFunction :: Int -> Int -> PixelRGB8
+renderFunction x y =  generatePixel (fst mappedPosition) (snd mappedPosition)
+    where
+        mappedPosition = mapToBoundries x y width height
